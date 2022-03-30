7e3f19ac397e94ab5a94301f55b297542bbcefa7
fhpg <67965190+fhpg@users.noreply.github.com>
2022-03-30 14:27:23 +0200

    better timings

--- ltewatchdog.sh
+++ ltewatchdog.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-. /etc/lte.config
 
 while [ "1" == "1" ] ; do
+  . /etc/lte.config
+
   PROFILE_PRESENT=$(qmicli -p -d $WDM --wds-get-profile-list=3gpp | grep internet.v6.telekom)
   if [ "$PROFILE_PRESENT" == "" ] ; then
     /opt/lte/lteinit.sh
+    sleep 10
+    /opt/lte/lteconn.sh
   fi
+
+  sleep 70
 
   IP4CONNECTED=$(ping -4 -c 1 $PING4HOST | grep " 0% packet loss")
   IP6CONNECTED=$(ping -6 -c 1 $PING6HOST | grep " 0% packet loss")
   if [[ "$IP4CONNECTED" == "" || "$IP6CONNECTED" == "" ]] ; then
     /opt/lte/ltedisconn.sh
+    sleep 20
     /opt/lte/lteconn.sh
   fi
-  sleep 70
 done
