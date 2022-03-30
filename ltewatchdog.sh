e58806e6c4f1dacec05e9b479dbbf80b65f53468
fhpg <67965190+fhpg@users.noreply.github.com>
2022-03-30 14:02:59 +0200

    auto startup and keep permanent connection

--- ltewatchdog.sh
+++ ltewatchdog.sh
@@ -2,7 +2,16 @@
 . /etc/lte.config
 
 while [ "1" == "1" ] ; do
-  ping -4 -c 1 $PING4HOST || (/opt/lte/ltedisconn.sh ; /opt/lte/lteconn.sh)
-  ping -6 -c 1 $PING6HOST || (/opt/lte/ltedisconn.sh ; /opt/lte/lteconn.sh)
-  sleep 300
+  PROFILE_PRESENT=$(qmicli -p -d $WDM --wds-get-profile-list=3gpp | grep internet.v6.telekom)
+  if [ "$PROFILE_PRESENT" == "" ] ; then
+    /opt/lte/lteinit.sh
+  fi
+
+  IP4CONNECTED=$(ping -4 -c 1 $PING4HOST | grep " 0% packet loss")
+  IP6CONNECTED=$(ping -6 -c 1 $PING6HOST | grep " 0% packet loss")
+  if [[ "$IP4CONNECTED" == "" || "$IP6CONNECTED" == "" ]] ; then
+    /opt/lte/ltedisconn.sh
+    /opt/lte/lteconn.sh
+  fi
+  sleep 70
 done
