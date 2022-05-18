a93a6e8e7efb94ef2f6aef4c680e14c35a08999b
fhpg <67965190+fhpg@users.noreply.github.com>
2022-05-18 18:13:25 +0200

    double check connection

--- ltewatchdog.sh
+++ ltewatchdog.sh
@@ -4,13 +4,17 @@
 
 while [ "1" == "1" ] ; do
   . /etc/lte.config
-  IP4CONNECTED=$(ping -4 -c 1 $PING4HOST | grep " 0% packet loss")
-  IP6CONNECTED=$(ping -6 -c 1 $PING6HOST | grep " 0% packet loss")
+  IP4CONNECTED=$(ping -4 -c 1 $PING4HOST1 | grep " 0% packet loss")
+  IP6CONNECTED=$(ping -6 -c 1 $PING6HOST1 | grep " 0% packet loss")
   if [[ "$IP4CONNECTED" == "" || "$IP6CONNECTED" == "" ]] ; then
-    date
-    /opt/lte/ltedisconn.sh
-    sleep 20
-    /opt/lte/lteconn.sh
+    IP4CONNECTED=$(ping -4 -c 1 $PING4HOST2 | grep " 0% packet loss")
+    IP6CONNECTED=$(ping -6 -c 1 $PING6HOST2 | grep " 0% packet loss")
+    if [[ "$IP4CONNECTED" == "" || "$IP6CONNECTED" == "" ]] ; then
+      date
+      /opt/lte/ltedisconn.sh
+      sleep 20
+      /opt/lte/lteconn.sh
+    fi
   fi
 
   sleep 70
